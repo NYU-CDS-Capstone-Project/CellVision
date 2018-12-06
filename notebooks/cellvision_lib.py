b1e352f030949197b23bc09feb9c6b6465512375
Godevais <dg3047@skynet.skynet.cluster>
2018-12-06 00:47:34 -0500

    Adding the new data split func

--- notebooks/cellvision_lib.py
+++ notebooks/cellvision_lib.py
@@ -1,89 +1,67 @@
-import numpy as np
-import pandas as pd
 import os
-from collections import defaultdict
+import glob
+import random
 
-def train_test_val(folder_path, channels = 1, train_pp = .6, test_pp = .2, val_pp = .2, set_seed = 1):
+def get_model_data_splits(imgs_dir, channel=1, train_pp=.6, test_pp=.2, val_pp=.2, seed=1):
+    """
+    Function which returns the a list containing a tuple of the channel image and its
+    matched reference channel for training, validation, and test.
+    
+    Expects filenames of the format: sample#_channel#_z#.jpg
+    
+    returns train, val, test
+    """
+    REF_CHANNEL = 6
+    random.seed(seed)
     #check to make sure percentages equal 1
     if train_pp+test_pp+val_pp != 1:
         raise Exception("train, test, val percentages must equal 1")
     
     #check to make sure channels in 1-5
-    for c in list((channels,6)):
-        if c not in list(range(1,7)):
-            raise Exception("channels can only be in range 1-5")
+    if channel > 5:
+        raise Exception("channels can only be in range 1-5")
     
-    #Find file paths
-    for root, dirs, files in os.walk(folder_path):
-        root_path = root
-        root_dir = dirs
-        root_files = files
+    img_paths = sorted(glob.glob(imgs_dir + '/*_channel{}_z*'.format(channel)))
+    ref_paths = sorted(glob.glob(imgs_dir + '/*_channel{}_z*'.format(REF_CHANNEL)))
+    random.shuffle(img_paths)
+    sample_prefixes = {}
+    for img_path in img_paths:
+        fname = os.path.basename(img_path)
+        end_sample_prefix_index = fname.find('_channel{}_z'.format(channel)) 
+        start_z_index = end_sample_prefix_index + 11
+        end_index = fname.find('.tif') 
+        sample_prefix = fname[0:end_sample_prefix_index]
+        z_depth = fname[start_z_index:end_index]
+        ref_path = '{}/{}_channel{}_z{}.tif'.format(imgs_dir, sample_prefix, REF_CHANNEL, z_depth)
+        if ref_path not in ref_paths:
+            print("WARNING! Missing ref for " + ref_path)
+            continue
+        if sample_prefix not in sample_prefixes:
+            sample_prefixes[sample_prefix] = [ (img_path, ref_path) ]
+        else:
+            sample_prefixes[sample_prefix] = sample_prefixes[sample_prefix] + [(img_path, ref_path)]
     
-    #split each file to find relevant numbers
-    sample_zplanes_folder = []
-    for file in root_files:
-        s,c,z = file.split('_')
-        s_n = s.strip('sample')
-        c_n = c.strip('channel')
-        z_n = z.strip('z').split('.')[0]
-        #create new entries where first part of key is sample #
-        #second part of key is zplane #
-        entry = list([str(s_n)+str('_')+str(z_n), file])
-        sample_zplanes_folder.append(entry)
+    sample_prefixes_lst = sorted(sample_prefixes.keys())
+    num_samples = len(sample_prefixes_lst)
+    random.shuffle(sample_prefixes_lst)
     
-    #create dictionary with new keys for sample # and zplane #
-    d = defaultdict(list)
-    for key, entry in sample_zplanes_folder:
-        d[key].append(entry)
+    train_end_idx = round(num_samples*train_pp)
+    val_end_idx = train_end_idx + round(num_samples*val_pp)
     
-    #full dictionary of files with corresponding sample/zplane #
-    samples = d
-    #just sample/zplane keys used to split data
-    samples_list = list(samples.keys())
-
-
-    #set number entries base on pp's for train, test, val
-    train_p, test_p, val_p = round((len(samples_list))*train_pp), \
-                             round((len(samples_list))*test_pp), \
-                             round((len(samples_list))*val_pp)
-
-    #set train seed
-    np.random.seed(set_seed)
-    #select training set
-    train = list(np.random.choice(samples_list, size=train_p, replace=False))
-    #remove training set from original list
-    samples_list = list(set(samples_list) - set(train))
-
-    #set test seed
-    np.random.seed(set_seed)
-    #select training set
-    test = list(np.random.choice(samples_list, size=test_p, replace=False))
-    #remove training set from original list
-    val = list(set(samples_list) - set(test))
+    train_samples = sample_prefixes_lst[0:train_end_idx]
+    val_samples = sample_prefixes_lst[train_end_idx:val_end_idx]
+    test_samples = sample_prefixes_lst[val_end_idx:]
     
-    #set channel list
-    channel_set = list((channels,6))
-    
-    def finalize_paths(split_set, samples):
-        paths = []
-        for t in split_set:
-            entry = samples[t]
-            paths.append(entry)
-
-        final = []
-        for file in paths:
-            temp = []
-            for channel_file in file:
-                s,c,z = channel_file.split('_')
-                c_n = c.strip('channel')
-                for c in channel_set:
-                    if c_n == str(c):
-                        temp.append('{}/{}'.format(root_path, channel_file))
-            final.append(sorted(temp))  
-        return final
-    
-    train_final = finalize_paths(train, samples)
-    test_final = finalize_paths(test, samples)
-    val_final = finalize_paths(val, samples)
-
-    return(train_final, test_final, val_final)
+    train = []
+    for sample in train_samples:
+        train = train + sample_prefixes[sample]
+        
+    val = []
+    for sample in val_samples:
+        val = val + sample_prefixes[sample]
+        
+    test = []
+    for sample in test_samples:
+        test = test + sample_prefixes[sample]
+        
+    return train, val, test
